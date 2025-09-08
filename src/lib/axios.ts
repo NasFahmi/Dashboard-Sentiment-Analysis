427baff9fb678f62b939d4d5b8ba7c5c0af88d5e
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-09-08 10:58:28 +0700

    set to http

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "https://chatbotumkm-backend-ocrh5p-46791d-84-247-151-112.traefik.me",
+  baseURL: "http://chatbotumkm-backend-ocrh5p-46791d-84-247-151-112.traefik.me",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
